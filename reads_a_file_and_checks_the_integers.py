2a76e2911778d9dffa67a3ab8bd051d4a60f61de
james malitan <jamesmalitan21@gmail.com>
2026-04-25 15:38:51 +0800

    making_the_blueprint_of_the_program

---
+++ reads_a_file_and_checks_the_integers.py
@@ -0,0 +1,7 @@
+class NumberSeperator:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.input_numbers = []
+        self.even_numbers = []
+        self.odd_numbers = []
+    
